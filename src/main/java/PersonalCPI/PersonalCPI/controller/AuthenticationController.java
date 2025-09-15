0c32ca2798322bd83c08c7f888e4321cc6216d7e
dfsaelee <dsaltac@gmail.com>
2025-09-15 10:09:24 -0700

    working authentication

---
+++ src/main/java/PersonalCPI/PersonalCPI/controller/AuthenticationController.java
@@ -0,0 +1,62 @@
+package PersonalCPI.PersonalCPI.controller;
+
+import PersonalCPI.PersonalCPI.dto.LoginUserDto;
+import PersonalCPI.PersonalCPI.dto.RegisterUserDto;
+import PersonalCPI.PersonalCPI.dto.VerifyUserDto;
+import PersonalCPI.PersonalCPI.model.User;
+import PersonalCPI.PersonalCPI.responses.LoginResponse;
+import PersonalCPI.PersonalCPI.service.AuthenticationService;
+import PersonalCPI.PersonalCPI.service.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        try {
+            User authenticatedUser = authenticationService.authenticate(loginUserDto); // will throw in authentication service if invalid
+            String jwtToken = jwtService.generateToken(authenticatedUser);
+            LoginResponse loginResponse = new LoginResponse(jwtService.getExpirationTime(), jwtToken);
+            return ResponseEntity.ok(loginResponse);
+        }
+        catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<?> verifyUser(@RequestBody VerifyUserDto verifyUserDto) {
+        try {
+            authenticationService.verifyUser(verifyUserDto);
+            return ResponseEntity.ok("Account Verified Successfully");
+        }
+        catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    @PostMapping("/resend")
+    public ResponseEntity<?> resendVerificationCode(@RequestParam String email) {
+        try {
+            authenticationService.resendVerificationCode(email);
+            return ResponseEntity.ok("verification code sent");
+        }
+        catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/PersonalCPI/PersonalCPI/PersonalCpiApplicationTests.java
@@ -0,0 +1,13 @@
+package PersonalCPI.PersonalCPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalCpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
